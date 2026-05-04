8a33ea3453ab02744a1b681cfedaae50451fc619
jose.araujo <jose.araujo@rnl.tecnico.ulisboa.pt>
2026-05-04 18:43:43 +0100

    initial code based on the slides

--- js/main-script.js
+++ js/main-script.js
@@ -1,21 +1,43 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { VRButton } from "three/addons/webxr/VRButton.js";
-import * as Stats from "three/addons/libs/stats.module.js";
-import { GUI } from "three/addons/libs/lil-gui.module.min.js";
+// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+// import { VRButton } from "three/addons/webxr/VRButton.js";
+// import * as Stats from "three/addons/libs/stats.module.js";
+// import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 
 //////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
+const HEIGHT = window.innerHeight;
+const WIDTH = window.innerWidth;
+const BACKGROUND = new THREE.Color(0xffffff);
+
+let camera;
+let renderer, scene;
+
+///////////////////////
+/* CLASS DEFINITIONS */
+///////////////////////
+//TODO
 
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
-function createScene() {}
+function createScene() {
+  scene = new THREE.Scene();
+  scene.background = BACKGROUND;
+
+  //TODO (ADD OBJECTS)
+}
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
+function setupCameras() {
+    // CHANGE! //TODO
+    camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 1000);
+    camera.position.set(0, 5, 10);
+    camera.lookAt(0, 0, 0);
+}
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -28,47 +50,87 @@ function createScene() {}
 //////////////////////
 /* CHECK COLLISIONS */
 //////////////////////
-function checkCollisions() {}
+function checkCollisions() {
+    //TODO
+    }
 
 ///////////////////////
 /* HANDLE COLLISIONS */
 ///////////////////////
-function handleCollisions() {}
+function handleCollisions() {
+    //TODO
+    }
+
+function toggleWireframe() {
+  scene.traverse((node) => {
+    if (node instanceof THREE.Mesh) node.material.wireframe = !node.material.wireframe;
+  });
+}
 
 ////////////
 /* UPDATE */
 ////////////
-function update() {}
+function update() {
+    //TODO
+    }
 
 /////////////
 /* DISPLAY */
 /////////////
-function render() {}
+function render() {
+  renderer.render(scene, camera);
+}
 
 ////////////////////////////////
 /* INITIALIZE ANIMATION CYCLE */
 ////////////////////////////////
-function init() {}
+function init() {
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(WIDTH, HEIGHT);
+  document.body.appendChild(renderer.domElement);
+
+  createScene();
+  setupCameras();
+
+  window.addEventListener("resize", onResize);
+  window.addEventListener("keydown", onKeyDown);
+  window.addEventListener("keyup", onKeyUp);
+}
 
 /////////////////////
 /* ANIMATION CYCLE */
 /////////////////////
-function animate() {}
+function animate() {
+  update();
+  requestAnimationFrame(animate);
+  render();
+}
 
 ////////////////////////////
 /* RESIZE WINDOW CALLBACK */
 ////////////////////////////
-function onResize() {}
+function onResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  if (window.innerHeight > 0 && window.innerWidth > 0) {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  }
+}
 
 ///////////////////////
 /* KEY DOWN CALLBACK */
 ///////////////////////
-function onKeyDown(e) {}
+function onKeyDown(e) {
+    //TODO
+    }
 
 ///////////////////////
 /* KEY UP CALLBACK */
 ///////////////////////
-function onKeyUp(e) {}
+function onKeyUp(e) {
+    //TODO
+    }
 
 init();
 animate();
